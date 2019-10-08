217e56941d6859a16c040ceb9f873fbf8a648e75
Catherine Quilley <catherine.quilley@nology.io>
2019-10-08 13:56:53 +0100

    implemented on click functionality so ship targets a random ship on button click. currently only outputs a console log

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 class Alien {
     hitPoints = 60;
-    damageTaken = 12;
-    canDie = true;    
+    damageTaken = 12; 
+    takeHit(){
+        console.log("OUCH")
+
+    } 
+    isDead = false; 
+    isEnemy = true;
 }
 
 class Worker extends Alien {
@@ -18,4 +23,75 @@ class Queen extends Alien{
     hitPoints = 80;
     damageTaken = 7;
 }
+
+class Ship {
+  
+    
+    fireAtAlien (){
+        this.alienSelected = Math.floor(Math.random() * 14) + 0;
+        console.log("boom");
+        return this.alienSelected;
+        
+
+    }
+
+   
+}
+
+
+
+    const battleStarGalatica = new Ship;
+    
+
+const createAliens = () => {
+
+    const numQueen = 1;
+    const numWorker = 5;
+    const numDrone = 8;
+
+    const brianMay = new Queen;
+    let queen = [];
+    let workers = [];
+    let drones = []; 
+    // const worker = new Worker;
+    
+    queen.push(brianMay);
+
+
+    for (let i = 0; i<numWorker; i++){
+        let worker = new Worker;
+        workers.push(worker);
+    }
+
+    
+    for (let i = 0; i<numDrone; i++){
+        let drone = new Drone;
+        drones.push(drone);
+        
+    }
+
+    
+    return [queen, workers, drones ].flat();
+}
+
+
+    
+
+const aliens = createAliens();
+
+
+const fire =(aliens)=> {
+    const listOfAliens = aliens;
+    let alienHit = battleStarGalatica.fireAtAlien();
+    console.log(alienHit);
+    //    listOfAliens[alienHit].takeHit();
+    //    console.log(listOfAliens[alienHit]);
+
+}
+// element = document.getElementById('ship');
+// console.log(element);
+// element.addEventListener('click', fire(aliens), false);
+// console.log(aliens);
+
+
 
