93380babd114a1f3d6e5385c893cbe48b571f140
Catherine Quilley <catherine.quilley@nology.io>
2019-10-08 10:57:37 +0100

    created initial classes

---
+++ main.js
@@ -0,0 +1,21 @@
+class Alien {
+    hitPoints = 60;
+    damageTaken = 12;
+    canDie = true;    
+}
+
+class Worker extends Alien {
+    hitPoints = 68;
+    damageTaken = 10;
+
+}
+
+class Drone extends Alien{
+
+}
+
+class Queen extends Alien{
+    hitPoints = 80;
+    damageTaken = 7;
+}
+
